05661a756107e398b70539d869297f7ff158cb45
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-31 22:41:36 -0700

    started component and css files

---
+++ src/DogList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class DogList extends Component {
+    render() {
+        return (
+            <div>
+                DogList Component
+            </div>
+        )
+    }
+}
+export default DogList;
---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Routes extends Component {
+    render() {
+        return (
+            <div>
+                Routes Component
+            </div>
+        )
+    }
+}
+export default Routes;
